fa2e5876c2b1675ac1a5ae0a6ddc44622024921c
Justin Sena <justin.sena44@gmail.com>
2024-09-14 00:11:49 -0400

    Ready for submission

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,1 +1,90 @@
+/*
 // Add all scripts to the JS folder
+function myFunc() {
+    var myDiv = document.getElementById("mydiv");
+    myDiv.innerHTML = "Hello World";
+}
+console.log('TESTING');
+*/
+
+//initialize function called when the script loads
+function initialize() {
+    createCitiesTable();
+}
+
+// Function to create a table with cities and their populations
+function createCitiesTable() {
+    var cityPop = [
+        {
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
+    ];
+   
+    // Create an HTML table element
+    var table = document.createElement("table");
+
+    // Create a header row
+    var headerRow = document.createElement("tr");
+
+    //add the "City" and "Population" columns to the header row
+    headerRow.insertAdjacentHTML("beforeend","<th>City</th><th>Population</th>");
+   
+    /*
+    // Add the "City" column
+    var cityHeader = document.createElement("th");
+    cityHeader.innerHTML = "City";
+    headerRow.appendChild(cityHeader);
+
+    // Add the "Population" column
+    var popHeader = document.createElement("th");
+    popHeader.innerHTML = "Population";
+    headerRow.appendChild(popHeader);
+    */
+
+    // Add the row to the table
+    table.appendChild(headerRow);
+
+    //loop to add a new row for each city
+    for(var i = 0; i < cityPop.length; i++){
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        table.insertAdjacentHTML('beforeend',rowHtml);
+    };
+    /*
+    // Loop to add a new row for each city
+    for (var i = 0; i < cities.length; i++) {
+        var tr = document.createElement("tr");
+
+        var city = document.createElement("td");
+        city.innerHTML = cities[i];
+        tr.appendChild(city);
+
+        var pop = document.createElement("td");
+        pop.innerHTML = population[i];
+        tr.appendChild(pop);
+
+        table.appendChild(tr);
+    }
+    */
+
+    // Add the table to the div in index.html
+    var myDiv = document.querySelector("#mydiv");
+    myDiv.appendChild(table);
+}
+
+// Call the initialize function when the window has loaded
+window.onload = initialize;
---
+++ boilerplate/js/main_Module_Follow_Thru.js
@@ -0,0 +1,92 @@
+var myDiv =  document.getElementById("mydiv");
+myDiv.innerHTML = "Hello World";
+
+//initialize function called when the script loads
+function initialize(){
+    cityPop();
+};
+
+//Me practicing the code for using one array of objects instead of two arrays.
+var cityPop = [
+    {
+        city: 'Madison',
+        population: 233209
+    },
+    {
+        city: 'Milwaukee',
+        population: 594833
+    },
+    {
+        city: 'Green Bay',
+        population: 104057
+    },
+    {
+        city: 'Superior',
+        population: 27244
+    }
+];
+
+console.log(cityPop[2])
+/*trying single array method
+
+//function to create a table with cities and their populations
+function cities(){
+    var cities = [
+        'Madison',
+        'Milwaukee',
+        'Green Bay',
+        'Superior'
+    ];
+    var population = [
+        233209,
+        594833,
+        104057,
+        27244
+    ];
+end of trying single array method */
+    //create an HTML table element
+    var table = document.createElement("table");
+
+    //create a header row
+    var headerRow = document.createElement("tr");
+
+
+/*
+These items here are using javascript language to write html language that creates the header row AND populates it with the string 
+at the end of the .innerHTML method.  It writes that string to the innerHTML element! Which is a thing, perhaps a node.
+*/
+    //add the "City" column
+    var cityHeader = document.createElement("th");
+    cityHeader.innerHTML = "City";
+    headerRow.appendChild(cityHeader);
+
+    //add the "Population" column
+    var popHeader = document.createElement("th");
+    popHeader.innerHTML = "Population";
+    headerRow.appendChild(popHeader);
+
+    //add the row to the table
+    table.appendChild(headerRow);
+
+    //loop to add a new row for each city
+    for (var i = 0; i < cityPop.length; i++){
+        var tr = document.createElement("tr");
+
+        var city = document.createElement("td");
+        city.innerHTML = cityPop[i];
+        tr.appendChild(city);
+
+        var pop = document.createElement("td");
+        pop.innerHTML = population[i];
+        tr.appendChild(pop);
+
+        table.appendChild(tr);
+    };
+
+    //add the table to the div in index.html
+    var myDiv =  document.getElementById("mydiv");
+    myDiv.appendChild(table);
+
+
+//call the initialize function when the window has loaded
+window.onload = initialize();
